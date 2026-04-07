825052b1b7672cf1f0ceb4dd5917c8227bea2486
reiz <150909903+reisz1995@users.noreply.github.com>
2026-04-07 09:44:52 -0300

    Update nbaUtils.ts

--- lib/nbaUtils.ts
+++ lib/nbaUtils.ts
@@ -102,9 +102,9 @@ export const calculateDeterministicPace = (
         const controlFactor = Math.min(0.30, Math.abs(defDelta) / 60); 
         
         if (defDelta > 0) {
-            projectedPace = (blendedPaceA * (5.5 + controlFactor)) + (blendedPaceB * (2.5 - controlFactor));
+            projectedPace = (blendedPaceA * (1.9 + controlFactor)) + (blendedPaceB * (0.9 - controlFactor));
         } else {
-            projectedPace = (blendedPaceA * (5.5 - controlFactor)) + (blendedPaceB * (2.5 + controlFactor));
+            projectedPace = (blendedPaceA * (1.9 - controlFactor)) + (blendedPaceB * (0.9 + controlFactor));
         }
     } else {
         projectedPace = (blendedPaceA + blendedPaceB) / 2;
